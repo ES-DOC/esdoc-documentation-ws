108926ee914355ed5d54c8aba3cb64963f991c18
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-01-06 13:18:15 +0100

    Removed obsolete commands.  Preparing for sub-shell paradigm.

---
+++ sh/db_flush.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Import utils.
+source $ESDOC_WS_HOME/bash/init.sh
+
+# Main entry point.
+main()
+{
+    log "API-DB: flushing $1 documents from api database ..."
+
+	activate_venv api
+	python $ESDOC_WS_HOME/sh/db_flush.py --project=$1 --source=$2
+
+    log "API-DB: flushed $1 documents from api database"
+}
+
+# Invoke entry point.
+main $1 $2
