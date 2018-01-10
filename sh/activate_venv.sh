d4f455f65e6c34d48dbbe62119deff7452479bea
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2018-01-10 11:00:52 +0100

    Simplified python path assignment

--- sh/activate_venv.sh
+++ sh/activate_venv.sh
@@ -6,8 +6,6 @@ source $ESDOC_WS_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	export PYTHONPATH=$PYTHONPATH:$ESDOC_WS_HOME
-	export PYTHONPATH=$PYTHONPATH:$PYESDOC_HOME
 	source $ESDOC_WS_HOME/ops/venv/bin/activate
 }
 
