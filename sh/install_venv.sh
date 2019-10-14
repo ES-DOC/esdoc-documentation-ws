6e5b5946d85fe67a4137e691a31a25ad11b064a2
Mark A. Greenslade <mark.greenslade@ieee.org>
2019-10-14 17:44:13 +0200

    1. Updated venv install/updates.

--- sh/install_venv.sh
+++ sh/install_venv.sh
@@ -14,7 +14,6 @@ main()
     source $ESDOC_WS_HOME/sh/activate_venv.sh
     pip2 install --upgrade pip
     pip2 install --upgrade --no-cache-dir -I -r $ESDOC_WS_HOME/resources/requirements.txt
-    pip2 install --upgrade --no-cache-dir -I -r $ESDOC_WS_HOME/resources/requirements-pyesdoc.txt
     deactivate
 }
 
